80e9c6f4f4ec2f2e00418a4c2bdaf37c0685fddc
brunoreis-dev <b.macedoreis@icloud.com>
2024-01-31 10:59:34 -0300

    to build the score points

--- assets/scripts/gameCtrl.ts
+++ assets/scripts/gameCtrl.ts
@@ -1,5 +1,16 @@
-import { _decorator, CCInteger, Component, Node } from "cc";
+import {
+  _decorator,
+  CCInteger,
+  Component,
+  input,
+  Node,
+  Input,
+  EventKeyboard,
+  KeyCode,
+  director,
+} from "cc";
 import { Ground } from "./ground";
+import { Results } from "./Results";
 const { ccclass, property } = _decorator;
 
 @ccclass("gameCtrl")
@@ -11,6 +22,12 @@ export class GameCtrl extends Component {
   public ground: Ground;
 
   @property({
+    type: Results,
+    tooltip: "results go here",
+  })
+  public result: Results;
+
+  @property({
     type: CCInteger,
   })
   public speed: number = 300;
@@ -20,9 +37,42 @@ export class GameCtrl extends Component {
   })
   public pipeSpeed: number = 200;
 
-  onLoad() {}
+  onLoad() {
+    this.initListener();
+    this.result.resetScore();
+    director.pause();
+  }
 
-  initListener() {}
+  initListener() {
+    input.on(Input.EventType.KEY_DOWN, this.onKeyDown, this);
+  }
 
-  startGame() {}
+  // Testing method DELETE me in final version
+  onKeyDown(event: EventKeyboard) {
+    switch (event.keyCode) {
+      case KeyCode.KEY_A:
+        this.gameOver();
+        break;
+      case KeyCode.KEY_P:
+        this.result.addScore();
+        break;
+      case KeyCode.KEY_Q:
+        this.resetGame();
+    }
+  }
+
+  startGame() {
+    this.result.hideResults();
+    director.resume();
+  }
+
+  gameOver() {
+    this.result.showResults();
+    director.pause();
+  }
+
+  resetGame() {
+    this.result.resetScore();
+    this.startGame();
+  }
 }
