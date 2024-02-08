9af03caf3fb02e7522872e8615951a0493314af8
brunoreis-dev <b.macedoreis@icloud.com>
2024-02-08 11:58:24 -0300

    to add hit triggers and audio in game

--- assets/scripts/gameCtrl.ts
+++ assets/scripts/gameCtrl.ts
@@ -8,11 +8,15 @@ import {
   EventKeyboard,
   KeyCode,
   director,
+  Contact2DType,
+  Collider2D,
+  IPhysics2DContact,
 } from "cc";
 import { Ground } from "./ground";
 import { Results } from "./Results";
 import { Bird } from "./Bird";
 import { PipePool } from "./PipePool";
+import { BirdAudio } from "./BirdAudio";
 const { ccclass, property } = _decorator;
 
 @ccclass("gameCtrl")
@@ -40,6 +44,11 @@ export class GameCtrl extends Component {
   public pipeQueue: PipePool;
 
   @property({
+    type: BirdAudio,
+  })
+  public clip: BirdAudio;
+
+  @property({
     type: CCInteger,
   })
   public speed: number = 300;
@@ -49,33 +58,45 @@ export class GameCtrl extends Component {
   })
   public pipeSpeed: number = 200;
 
+  public isOver: boolean;
+
   onLoad() {
     this.initListener();
     this.result.resetScore();
+    this.isOver = true;
     director.pause();
   }
 
   initListener() {
-    input.on(Input.EventType.KEY_DOWN, this.onKeyDown, this);
+    // input.on(Input.EventType.KEY_DOWN, this.onKeyDown, this);
     this.node.on(Node.EventType.TOUCH_START, () => {
-      this.bird.fly();
+      if (this.isOver == true) {
+        this.resetGame();
+        this.bird.resetBird();
+        this.startGame();
+      }
+
+      if (this.isOver == false) {
+        this.bird.fly();
+        this.clip.onAudioQueue(0);
+      }
     });
   }
 
   // Testing method DELETE me in final version
-  onKeyDown(event: EventKeyboard) {
-    switch (event.keyCode) {
-      case KeyCode.KEY_A:
-        this.gameOver();
-        break;
-      case KeyCode.KEY_P:
-        this.result.addScore();
-        break;
-      case KeyCode.KEY_Q:
-        this.resetGame();
-        this.bird.resetBird();
-    }
-  }
+  // onKeyDown(event: EventKeyboard) {
+  //   switch (event.keyCode) {
+  //     case KeyCode.KEY_A:
+  //       this.gameOver();
+  //       break;
+  //     case KeyCode.KEY_P:
+  //       this.result.addScore();
+  //       break;
+  //     case KeyCode.KEY_Q:
+  //       this.resetGame();
+  //       this.bird.resetBird();
+  //   }
+  // }
 
   startGame() {
     this.result.hideResults();
@@ -84,20 +105,55 @@ export class GameCtrl extends Component {
 
   gameOver() {
     this.result.showResults();
+    this.isOver = true;
+    this.clip.onAudioQueue(3);
     director.pause();
   }
 
   resetGame() {
     this.result.resetScore();
     this.pipeQueue.reset();
+    this.isOver = false;
     this.startGame();
   }
 
   passPipe() {
     this.result.addScore();
+    this.clip.onAudioQueue(1);
   }
 
   createPipe() {
     this.pipeQueue.addPool();
+  }
+
+  contactGroundPipe() {
+    let collider = this.bird.getComponent(Collider2D);
+
+    if (collider) {
+      collider.on(Contact2DType.BEGIN_CONTACT, this.onBeginContact, this);
+    }
+  }
+
+  onBeginContact(
+    selfCollider: Collider2D,
+    otherCollider: Collider2D,
+    contact: IPhysics2DContact | null
+  ) {
+    this.bird.hitSomething = true;
+    this.clip.onAudioQueue(2);
+  }
+
+  birdStruck() {
+    this.contactGroundPipe();
+
+    if (this.bird.hitSomething == true) {
+      this.gameOver();
+    }
+  }
+
+  update() {
+    if (this.isOver == false) {
+      this.birdStruck();
+    }
   }
 }
